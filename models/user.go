b21a648e12f87270c182b7e7b796e572e5b63fb7
redblood639 <albinorabbithunter@gmail.com>
2023-04-08 09:37:08 -0400

    update : add the comment for gorm.Model

--- models/user.go
+++ models/user.go
@@ -7,11 +7,11 @@ import (
 
 // This struct will be used as like table in database.
 type User struct {
-	gorm.Model
-	Name     string `json:"name"`
-	Username string `json:"username" gorm:"unique"`
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
+	gorm.Model        // The gorm.Model specification adds some default properties to the Model, like id, created date, modified date, and deleted date.
+	Name       string `json:"name"`
+	Username   string `json:"username" gorm:"unique"`
+	Email      string `json:"email" gorm:"unique"`
+	Password   string `json:"password"`
 }
 
 func (user *User) HashPassword(password string) error {
